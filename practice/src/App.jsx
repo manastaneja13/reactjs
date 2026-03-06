1db74c629a8cb614ee07b8412ab9c2c44ea2ae9f
Manas Taneja <manastaneja1303@gmail.com>
2026-03-06 14:08:03 +0530

    practice done

---
+++ practice/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div id= 'papa'>
+      <h1>Manas </h1>
+      <h2>  hiiiii  </h2>
+    </div>
+    <div id = 'chacha'>
+      <h1>chacha wowww</h1>
+    </div>
+  </>
+  )
+}
+
+export default App
